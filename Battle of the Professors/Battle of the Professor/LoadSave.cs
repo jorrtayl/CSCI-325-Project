4b0c5f5970bfcf33739593b291d2d54ccadd4514
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-04-09 16:37:54 -0500

    Added header comments for both files.

---
+++ Battle of the Professors/Battle of the Professor/LoadSave.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Mason and Jordan
+ * Date: 03 / 28 / 2021
+ * Description: Loads and saves the game.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_of_the_Professor
+{
+    class LoadSave
+    {
+
+    }
+}
