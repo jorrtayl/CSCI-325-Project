d3c521d4a54ac2feeab504fbe7bd5ad720ba71a8
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-04-09 16:37:54 -0500

    Added header comments for both files.

--- Battle of the Professors/Battle of the Professor/MainWindow.xaml.cs
+++ Battle of the Professors/Battle of the Professor/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Author: Mason and Jordan
+ * Date: 03 / 28 / 2021
+ * Description: 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
